6dbd90e11a604a935d381b28bfe7973cbbfb9362
Ffjt <fjt@qq.com>
2024-04-27 20:07:13 +0800

    first commit

---
+++ src/main/java/com/fjt/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.fjt.git;
+
+/**
+ * @author ffjt
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello, git");
+    }
+}
